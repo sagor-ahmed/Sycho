8aa42a68a895888dc3358494e41039e927090192
sagor-ahmed <sagorahmed00999@gmail.com>
2021-06-02 22:28:23 +0630

    prob fix

--- js/main.js
+++ js/main.js
@@ -1,11 +1,8 @@
-
-
-
 (function ($) {
 
-"use strict"
-    
-    
+    "use strict"
+
+
     ////////////////////////////////////////////////////
     //  Scroll To Top Js
 
@@ -68,263 +65,263 @@
 
     });
 
-    
-    
-    
- ////////////////////////////////////////////////////
+
+
+
+    ////////////////////////////////////////////////////
 
     //  Project Slider Js
 
 
-	$('.project-slider-active').owlCarousel({
+    $('.project-slider-active').owlCarousel({
 
-		loop:true,
+        loop: true,
 
-		margin:20,
+        margin: 20,
 
-		autoplay:false,
+        autoplay: false,
 
-		autoplayTimeout:3000,
+        autoplayTimeout: 3000,
 
-		smartSpeed:500,
+        smartSpeed: 500,
 
-		items:6,
+        items: 6,
 
-		navText:['<button><i class="fal fa-angle-left"></i></button>','<button><i class="fal fa-angle-right"></i></button>'],
+        navText: ['<button><i class="fal fa-angle-left"></i></button>', '<button><i class="fal fa-angle-right"></i></button>'],
 
-		nav:true,
+        nav: true,
 
-		dots:false,
+        dots: false,
 
-		responsive:{
+        responsive: {
 
-			0:{
+            0: {
 
-				items:1
+                items: 1
 
-			},
+            },
 
-			767:{
+            767: {
 
-				items:2
+                items: 2
 
-			},
+            },
 
-			992:{
+            992: {
 
-				items:2
+                items: 2
 
-			},
+            },
 
-			1200:{
+            1200: {
 
-				items:3
+                items: 3
 
-			},
+            },
 
-			1600:{
+            1600: {
 
-				items:4
+                items: 4
 
-			}
+            }
 
-		}
+        }
 
-	});
-    
-  
-	////////////////////////////////////////////////////
+    });
+
+
+    ////////////////////////////////////////////////////
+
+    //  Post Box Slider Js
+
+    $('.postbox-gallery').owlCarousel({
+
+        loop: true,
+
+        margin: 0,
+
+        autoplay: false,
+
+        autoplayTimeout: 3000,
+
+        smartSpeed: 500,
+
+        items: 6,
+
+        navText: ['<button><i class="fas fa-arrow-left"></i></button>', '<button><i class="fas fa-arrow-right"></i></button>'],
+
+        nav: true,
+
+        dots: false,
+
+        responsive: {
+
+            0: {
+
+                items: 1
+
+            },
+
+            767: {
+
+                items: 1
+
+            },
+
+            992: {
+
+                items: 1
+
+            }
+
+        }
+
+    });
+
+
+
+    ////////////////////////////////////////////////////
 
     //  Post Box Slider Js
 
-	$('.postbox-gallery').owlCarousel({
+    $('.testimonial-slider-2-active').owlCarousel({
 
-		loop:true,
+        loop: true,
 
-		margin:0,
+        margin: 30,
 
-		autoplay:false,
+        autoplay: false,
 
-		autoplayTimeout:3000,
+        autoplayTimeout: 3000,
 
-		smartSpeed:500,
+        smartSpeed: 500,
 
-		items:6,
+        items: 6,
 
-		navText:['<button><i class="fas fa-arrow-left"></i></button>','<button><i class="fas fa-arrow-right"></i></button>'],
+        navText: ['<button><i class="fas fa-angle-left"></i></button>', '<button><i class="fas fa-angle-right"></i></button>'],
 
-		nav:true,
+        nav: true,
 
-		dots:false,
+        dots: false,
 
-		responsive:{
+        responsive: {
 
-			0:{
+            0: {
+                items: 1
+            },
 
-				items:1
+            767: {
 
-			},
+                items: 1
 
-			767:{
+            },
 
-				items:1
+            992: {
+                items: 2
+            }
+        }
+    });
 
-			},
 
-			992:{
 
-				items:1
 
-			}
-
-		}
-
-	});
-
-    
-    
-	////////////////////////////////////////////////////
-
-    //  Post Box Slider Js
-    
-	$('.testimonial-slider-2-active').owlCarousel({
-
-		loop:true,
-
-		margin:30,
-
-		autoplay:false,
-
-		autoplayTimeout:3000,
-
-		smartSpeed:500,
-
-		items:6,
-
-		navText:['<button><i class="fas fa-angle-left"></i></button>','<button><i class="fas fa-angle-right"></i></button>'],
-
-		nav:true,
-
-		dots:false,
-
-		responsive:{
-
-			0:{
-				items:1
-			},
-
-			767:{
-
-				items:1
-
-			},
-
-			992:{
-				items:2
-			}
-		}
-	});
-
-    
-    
-    
-	////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////
 
     // Sidebar Js
-   
-    
-   $(".info-toggle-btn").click(function(){  
-       $(".info-area").addClass("info-opened");  
-   }); 
 
-    $(".info-close-icon").click(function(){                            
-       $(".info-area").removeClass("info-opened");   
-    });
-    
-   $(".info-toggle-btn").click(function(){                            
-       $(".body-overlay").addClass("opened");   
-    });
-    
-     $(".info-close-icon").click(function(){                            
-       $(".body-overlay").removeClass("opened");   
-    });
-    
 
-    
-    
+    $(".info-toggle-btn").click(function () {
+        $(".info-area").addClass("info-opened");
+    });
+
+    $(".info-close-icon").click(function () {
+        $(".info-area").removeClass("info-opened");
+    });
+
+    $(".info-toggle-btn").click(function () {
+        $(".body-overlay").addClass("opened");
+    });
+
+    $(".info-close-icon").click(function () {
+        $(".body-overlay").removeClass("opened");
+    });
+
+
+
+
     /* ===== Counter Up Plugin ==== */
 
     $('.counter').counterUp({
         delay: 2,
         time: 500
     });
-    
+
     $('.counters').counterUp({
         delay: 2,
         time: 500
     });
-   
-   $(".has-dropdown>a").click(function(e) {
-                e.preventDefault();
-              
-           $(this).parent().siblings(".has-dropdown").children("ul").slideUp(200);
-                $(this).next().slideToggle(200);
-            });  
-    
-    
 
-  //SMOOTH SCROLL
-    $('.main-menu li a[href^="#"]').on('click', function(e){
-          e.preventDefault();
+    $(".has-dropdown>a").click(function (e) {
+        e.preventDefault();
 
-          var target = $(this.hash);
+        $(this).parent().siblings(".has-dropdown").children("ul").slideUp(200);
+        $(this).next().slideToggle(200);
+    });
 
-          if (target.length) {
-              $('html, body').stop().animate({
-                  scrollTop: target.offset().top -60
-              }, 2000);
-          }
-      });
-  
 
-    
-    
-    
-$('.portfolio-active').slick({
-  slidesToShow: 3,
-  slidesToScroll: 3,
-    arrows: false,
-    infinite: true,
-    centerMode: true,
-    centerPadding: "-100px",
-  responsive: [
-    {
-      breakpoint: 1200,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2,
+
+    //SMOOTH SCROLL
+    $('.main-menu li a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+
+        var target = $(this.hash);
+
+        if (target.length) {
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top - 60
+            }, 2000);
+        }
+    });
+
+
+
+
+
+    $('.portfolio-active').slick({
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        arrows: false,
         infinite: true,
-        dots: false,
-      }
+        centerMode: true,
+        centerPadding: "-100px",
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    infinite: true,
+                    dots: false,
+                }
     },
-  
-    {
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
+
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
     }
   ]
-});
+    });
 
-    
-   /* // One Page Nav
-    var top_offset = $('.header-area').height() - 10;
-    $('.main-menu nav ul').onePageNav({
-        currentClass: 'active',
-        scrollOffset: top_offset,
-    });*/
+
+    /* // One Page Nav
+     var top_offset = $('.header-area').height() - 10;
+     $('.main-menu nav ul').onePageNav({
+         currentClass: 'active',
+         scrollOffset: top_offset,
+     });*/
 
     // mainSlider
 
@@ -374,7 +371,7 @@ $('.portfolio-active').slick({
     mainSlider();
 
 
-// owlCarousel
+    // owlCarousel
     $('.owl-sagor').owlCarousel({
         loop: true,
         margin: 0,
@@ -396,117 +393,151 @@ $('.portfolio-active').slick({
     })
 
 
-    
 
-   	$(".client-slider-active").owlCarousel({
-		items: 5,
-		dots: false,
+
+    $(".client-slider-active").owlCarousel({
+        items: 5,
+        dots: false,
         loop: true,
         autoplay: true,
-		margin: 70,
-		responsive: {		
-			0:{items: 1},
-			480:{items: 1},
-			768:{items:1},
-			992:{items: 3},
-			1200:{items:4},
-            1600: {items:5}
-		},
-		
-	});
-    
+        margin: 70,
+        responsive: {
+            0: {
+                items: 1
+            },
+            480: {
+                items: 1
+            },
+            768: {
+                items: 1
+            },
+            992: {
+                items: 3
+            },
+            1200: {
+                items: 4
+            },
+            1600: {
+                items: 5
+            }
+        },
+
+    });
+
     //footer top award slider
-    
-  	$(".award-slider-active").owlCarousel({
-		items: 5,
-		dots: false,
+
+    $(".award-slider-active").owlCarousel({
+        items: 5,
+        dots: false,
         loop: true,
-		 margin: 30,
-		responsive: {
-			
-			0:{items: 2},
-			767:{items: 2},
-			768:{items:3},
-			992:{items: 3},
-			1092:{items:4},
-            1600:{iems:5}
-		},
-		
-	});
-    
- 	////////////////////////////////////////////////////
+        margin: 30,
+        responsive: {
+
+            0: {
+                items: 2
+            },
+            767: {
+                items: 2
+            },
+            768: {
+                items: 3
+            },
+            992: {
+                items: 3
+            },
+            1092: {
+                items: 4
+            },
+            1600: {
+                iems: 5
+            }
+        },
+
+    });
+
+    ////////////////////////////////////////////////////
 
     // 0. Testimonial Slider 3 Js
 
-	$('.test-slider').owlCarousel({
+    $('.test-slider').owlCarousel({
 
-		loop:true,
-
-		margin:0,
-
-		autoplay:false,
-
-		autoplayTimeout:3000,
-
-		smartSpeed:500,
-
-		items:6,
-       navText:['<button><i class="fas fa-long-arrow-alt-left"></i></button>','<button><i class="fas fa-long-arrow-alt-left"></i></button>'],
-
-		nav:true,
-
-		dots:false,
-
-		responsive:{
-
-			0:{
-
-				items:1
-
-			},
-
-			767:{
-
-				items:1
-
-			},
-
-			992:{
-
-				items:1
-
-			}
-
-		}
-
-	});
-
-    
-    
-    
-    
-   //testimonial slider 
-    
-       	$(".testimonial-slider").owlCarousel({
-		items: 3,
-		margin: 35,
         loop: true,
-		nav: true,
+
+        margin: 0,
+
+        autoplay: false,
+
+        autoplayTimeout: 3000,
+
+        smartSpeed: 500,
+
+        items: 6,
+        navText: ['<button><i class="fas fa-long-arrow-alt-left"></i></button>', '<button><i class="fas fa-long-arrow-alt-left"></i></button>'],
+
+        nav: true,
+
+        dots: false,
+
+        responsive: {
+
+            0: {
+
+                items: 1
+
+            },
+
+            767: {
+
+                items: 1
+
+            },
+
+            992: {
+
+                items: 1
+
+            }
+
+        }
+
+    });
+
+
+
+
+
+    //testimonial slider 
+
+    $(".testimonial-slider").owlCarousel({
+        items: 3,
+        margin: 35,
+        loop: true,
+        nav: true,
         dots: false,
         navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
-		responsive: {
-			0:{items: 1},
-			480:{items: 1},
-			768:{items:1},
-			992:{items: 2},
-			1200:{items:3},
-            
-		},
-	});
+        responsive: {
+            0: {
+                items: 1
+            },
+            480: {
+                items: 1
+            },
+            768: {
+                items: 1
+            },
+            992: {
+                items: 2
+            },
+            1200: {
+                items: 3
+            },
 
-    
+        },
+    });
+
+
     /* magnificPopup img view */
-    
+
     $('.popup-image').magnificPopup({
         type: 'image',
         gallery: {
@@ -522,55 +553,57 @@ $('.portfolio-active').slick({
 
 
 
-	////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////
 
     //  Masonary Js
 
-	var $grid=  $('.grid').imagesLoaded( function() {
+    var $grid = $('.grid').imagesLoaded(function () {
 
-		// init Isotope
+        // init Isotope
 
-		var $grid = $('.grid').isotope({
+        var $grid = $('.grid').isotope({
 
-		  itemSelector: '.grid-item',
+            itemSelector: '.grid-item',
 
-		  percentPosition: true,
+            percentPosition: true,
 
-		  masonry: {
+            masonry: {
 
-			// use outer width of grid-sizer for columnWidth
+                // use outer width of grid-sizer for columnWidth
 
-			columnWidth: '.grid-item',
+                columnWidth: '.grid-item',
 
-		  }
+            }
 
-		});
+        });
 
     });
 
-	// filter items on button click
+    // filter items on button click
 
-	$('.masonary-menu').on( 'click', 'button', function() {
+    $('.masonary-menu').on('click', 'button', function () {
 
-	  var filterValue = $(this).attr('data-filter');
+        var filterValue = $(this).attr('data-filter');
 
-	  $grid.isotope({ filter: filterValue });
+        $grid.isotope({
+            filter: filterValue
+        });
 
-	});
+    });
 
 
 
-	//for menu active class
+    //for menu active class
 
-	$('.masonary-menu button').on('click', function(event) {
+    $('.masonary-menu button').on('click', function (event) {
 
-		$(this).siblings('.active').removeClass('active');
+        $(this).siblings('.active').removeClass('active');
 
-		$(this).addClass('active');
+        $(this).addClass('active');
 
-		event.preventDefault();
+        event.preventDefault();
 
-	});
+    });
 
 
     // filter items on button click
@@ -589,7 +622,7 @@ $('.portfolio-active').slick({
     });
 
 
-  // WOW active
+    // WOW active
     new WOW().init();
 
 
@@ -614,24 +647,24 @@ const navbar = document.getElementById("nav");
 const topLink = document.querySelector(".top-link");
 
 window.addEventListener("scroll", function () {
- const scrollHeight = window.pageYOffset;
-    
-  const navHeight = navbar.getBoundingClientRect().height;
-  if (scrollHeight > navHeight) {
-    navbar.classList.add("fixed-nav");
-  } else {
-    navbar.classList.remove("fixed-nav");
-  }
-/*  //setup back to top link
-  
-  if (scrollHeight > 500) {
-    topLink.classList.add("show-link");
-  }
-    
-    else {
-    topLink.classList.remove("show-link");
-  }*/
-    
+    const scrollHeight = window.pageYOffset;
+
+    const navHeight = navbar.getBoundingClientRect().height;
+    if (scrollHeight > navHeight) {
+        navbar.classList.add("fixed-nav");
+    } else {
+        navbar.classList.remove("fixed-nav");
+    }
+    /*  //setup back to top link
+      
+      if (scrollHeight > 500) {
+        topLink.classList.add("show-link");
+      }
+        
+        else {
+        topLink.classList.remove("show-link");
+      }*/
+
 });
 
 
@@ -642,25 +675,3 @@ window.addEventListener("scroll", function () {
 // add - add class
 // remove - remove class
 // toggle - toggles class
-
-
-
-
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
